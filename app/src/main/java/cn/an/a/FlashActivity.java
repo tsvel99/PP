ba5c1880b2f9a62eab06f364ccee54d9f109c497
tsvel-99 <ta333s@163.com>
2022-02-07 17:12:59 +0800

    Initial commit

--- app/src/main/java/cn/an/a/FlashActivity.java
+++ app/src/main/java/cn/an/a/FlashActivity.java
@@ -4,13 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FlashActivity extends AppCompatActivity {
+import java.util.List;
 
+import cn.an.bean.UpData;
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+
+public class FlashActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flash_activity);
+        //初始化bmob
+        Bmob.initialize(this,"4dcbf5e073fc99313244ea0f5ed03db9");
+
+        BmobQuery<UpData> query=new BmobQuery<>();
+        query.findObjects(new FindListener<UpData>() {
+            @Override
+            public void done(List<UpData> list, BmobException e) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/cn/an/bean/T.java
@@ -0,0 +1,28 @@
+package cn.an.bean;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+public class T {
+
+    //吐司
+    public void toast(Context context,String str){
+        Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
+    }
+
+    public static String packageName(Context context) {
+        PackageManager manager = context.getPackageManager();
+        String name = null;
+        try {
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            name = info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return name;
+    }
+
+}
